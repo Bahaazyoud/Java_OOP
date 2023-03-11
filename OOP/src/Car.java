135828b6116e8433a0fb822b0125f1d56c25896d
Bahaazyoud <bahaalzyoud@digisolfze.com>
2023-03-11 20:04:08 +0300

    oop decalre object from class

---
+++ OOP/src/Car.java
@@ -0,0 +1,12 @@
+public class Car {
+	String make = "kia";
+	String model = "Seltos";
+	int year = 2019;
+	String color = "red";
+	double price = 20000.00;
+	
+	
+	void drive() {
+		System.out.println("you drive the car");
+	}
+}
