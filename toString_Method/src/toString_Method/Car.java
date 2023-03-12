ece47a3d49e72557f225a755170179f35b1135aa
Bahaazyoud <bahaalzyoud@digisolfze.com>
2023-03-12 23:44:04 +0300

    toString method

---
+++ toString_Method/src/toString_Method/Car.java
@@ -0,0 +1,10 @@
+package toString_Method;
+
+public class Car {
+	String color = "red";
+	double price = 1000.00;
+	
+	public String toString() {
+		return color + "\n" + price ;
+	}
+}
---
+++ toString_Method/src/toString_Method/Main.java
@@ -0,0 +1,10 @@
+package toString_Method;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Car car = new Car();
+		System.out.println(car.toString());
+	}
+
+}
